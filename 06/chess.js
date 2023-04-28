114d90cacdd3efafbafdd192673ea190241005dd
kimmjae99 <kimmjae99@gmail.com>
2023-04-28 16:01:13 +0900

    8주차 내용

--- 06/chess.js
+++ 06/chess.js
@@ -1,45 +1,42 @@
 const $board = document.getElementById('board');
-
-function createBoard(){
-    const div = '<div class="${color}"></div>';
-    return div;
-}
-
-function createRow(count=4, startColor){
-    let row = '';
-    for(let i = 0; i < count; i++){
-        let color = '';
-        if(i % 2 ==0){
-            color = startColor ==='white' ? 'black' : 'white';
-        }
-        row += createBoard(startColor);
-    }
-}
-
-function createChessBoard(count=4){
-    let board = '';
-    for(let i = 0; i < count; i++){
-        const startColor = i % 2 == 0 ? 'black' : 'white';
-        board += createRow(4, startColor);
-    }
-    return board;
-}
-
+const WHITE_COLOR = 'white';
+const BLACK_COLOR = 'black';
 let $prevTarget = '';
-$board.addEventListener('click',selectBoard());
-$board.innerHTML = createChessBoard();
 
-function selectBoard() {
-    return event => {
-        const $target = event.target;
-        //$target.className += ' red';
-        if ($target == $prevTarget)
-            return;
-        $target.classList.add('red');
+const createBoard = color => `<div class="${color}"></div>`;
 
+const createRow = (count = 4, startColor = 'white') => {
+  let row = '';
+  for (let i = 0; i < count; i++) {
+    let color = startColor;
+    if (i % 2 == 0) {
+      color = startColor === WHITE_COLOR ? BLACK_COLOR : WHITE_COLOR;
+    }
+    row += createBoard(color);
+  }
+  return row;
+};
 
-        $prevTarget && $prevTarget.classList.remove('red');
+const createChessBoard = (count = 4) => {
+  let board = '';
+  for (let i = 0; i < count; i++) {
+    const startColor = i % 2 === 0 ? WHITE_COLOR : BLACK_COLOR;
+    board += createRow(4, startColor);
+  }
+  return board;
+};
 
-        const $prevTarget = $target;
-    };
-}
+const selectBoard = event => {
+  const $target = event.target;
+  if ($target === $prevTarget) return;
+  $target.classList.add('red');
+  $prevTarget && $prevTarget.classList.remove('red');
+  $prevTarget = $target;
+};
+
+const init = () => {
+  $board.addEventListener('click', selectBoard);
+  $board.innerHTML = createChessBoard();
+};
+
+init();
