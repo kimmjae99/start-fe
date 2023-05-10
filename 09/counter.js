e7eadae82e2edc0434b43fc0ba1be0c65a785dc3
kimmjae99 <kimmjae99@gmail.com>
2023-05-10 21:42:24 +0900

    9주차 내용

---
+++ 09/counter.js
@@ -0,0 +1,24 @@
+import Button from './Button.js';
+import Wrapper from './Wrapper.js';
+
+const $root = document.querySelector('#root');
+
+let count = 0;
+function plus() {
+  console.log('plus');
+  setCount(++count);
+}
+
+function minus() {
+  console.log('minus');
+  setCount(--count);
+}
+
+const $plusButton = Button({ text: 'plus', onClick: plus });
+const $minusButton = Button({ text: 'minus', onClick: minus });
+const { $wrapper: $countWrapper, setText: setCount } = Wrapper({
+  text: 0,
+  className: 'count',
+});
+
+$root.append($countWrapper, $plusButton, $minusButton);
